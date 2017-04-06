5621376273803b0d80b2ad735b346305dc05ed69
OpalCat <noblesse3539@daum.net>
2017-04-06 19:36:38 +0900

    add postfix() at CalcApp.java

--- src/main/java/com/riotgames/sample/CalcApp.java
+++ src/main/java/com/riotgames/sample/CalcApp.java
@@ -1,11 +1,16 @@
 package com.riotgames.sample;
 
 import java.util.Arrays;
+import java.util.Stack;
+
 
 /**
  * Calculator application
  */
 public class CalcApp {
+	private Stack<String> oStack;
+	private String[] postfix;
+	
     public double calc(String[] tokens) {
         final double firstOperand;
         final double secondOperand;
@@ -20,6 +25,87 @@ public class CalcApp {
         return operator.evaluate(firstOperand, secondOperand);
 
     }
+    public boolean isNumeric(String s) {		// String 파라미터가 숫자인지 판별
+    	  try {
+    	      Double.parseDouble(s);
+    	      return true;
+    	  } catch(NumberFormatException e) {
+    	      return false;
+    	  }
+    }
+    public boolean token2Postfix(String[] tokens) {
+		int i = 0;
+		int p = 0;
+		String curToken, poppedToken, topToken;
+
+		this.oStack = new Stack<String>();
+		this.postfix = new String[tokens.length];
+
+		while (i < tokens.length) {
+			curToken = tokens[i++];
+			if (this.isNumeric(curToken) == true) {
+				this.postfix[p++] = curToken;
+			}else {
+				if (curToken.charAt(0) == ')') {
+					if (!this.oStack.isEmpty())
+						poppedToken = this.oStack.pop();
+					else
+						return false;
+
+					while (poppedToken.charAt(0) != '(') {
+						this.postfix[p++] = poppedToken;
+						if (!this.oStack.isEmpty())
+							poppedToken = this.oStack.pop();
+						else
+							return false;
+					}
+				} else {
+					int inComingP = inComingPrecedence(curToken);
+					if (!this.oStack.isEmpty()) {
+						topToken = this.oStack.peek();
+						while (inStackPrecedence(topToken) >= inComingP) {
+							poppedToken = this.oStack.pop();
+							this.postfix[p++] = poppedToken;
+							if (!this.oStack.isEmpty())
+								topToken = this.oStack.peek();
+							else
+								break;
+						}
+					}
+					this.oStack.push(curToken);
+				}
+			}
+		}
+		while(!this.oStack.isEmpty()){
+			poppedToken = this.oStack.pop();
+			this.postfix[p++] = poppedToken;
+		}
+		return true;
+	}
+    private int inComingPrecedence(String givenToken) {
+		if (givenToken.charAt(0) == '+' || givenToken.charAt(0) == '-')
+			return 12;
+		else if (givenToken.charAt(0) == '*' || givenToken.charAt(0) == '/')
+			return 13;
+		else if (givenToken.charAt(0) == '(')
+			return 20;
+		else if (givenToken.charAt(0) == ')')
+			return 19;
+		else
+			return 0;
+	}
+    private int inStackPrecedence(String givenToken) {
+		if (givenToken.charAt(0) == '+' || givenToken.charAt(0) == '-')
+			return 12;
+		else if (givenToken.charAt(0) == '*' || givenToken.charAt(0) == '/')
+			return 13;
+		else if (givenToken.charAt(0) == '(')
+			return 0;
+		else if (givenToken.charAt(0) == ')')
+			return 19;
+		else
+			return 0;
+	}
 
     public static void main( String[] args ) {
         final CalcApp app = new CalcApp();
@@ -27,5 +113,7 @@ public class CalcApp {
         Arrays.asList(args).forEach(value -> outputs.append(value + " "));
         System.out.print( "Addition of values: " + outputs + " = ");
         System.out.println(app.calc(args));
+        System.out.println("");
+
     }
 }
